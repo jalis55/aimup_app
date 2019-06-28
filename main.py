dfd9eb2f4e821d9fb82a3dd5552b2c9c8a79cd54
jalis55 <jalismahamud2055@gmail.com>
2019-06-28 15:40:11 +0600

    upto database connection

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
 
 
 app=Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/aimup_app"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Desktip/saari/Aimup_app/aimup_app.db"
 db = SQLAlchemy(app)
+
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(50),nullable=False)
+	last_name = db.Column(db.String(50),nullable=False)
+	email=db.Column(db.String(50),nullable=False)
+	profile_picture=db.Column(db.String(100))
+	description=db.Column(db.String(100))
+	password=db.Column(db.String(100),nullable=False)
+
+
+
 
 @app.route('/')
 def home():
@@ -19,6 +33,19 @@ def login():
 @app.route('/signup')
 def signup():
 	return render_template('signup.html')
+@app.route('/userSignUp',methods=['POST'])
+def user_sign_up():
+	email=request.form['email']
+	first_name=request.form['first_name']
+	last_name=request.form['last_name']
+	password=request.form['password']
+
+	create_user=Users(email=email,first_name=first_name,last_name=last_name,password=password)
+	db.session.add(create_user)
+	db.session.commit()
+	
+
+	return "sign up successful"
 
 
 
